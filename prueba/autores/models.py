b3f6dda803a30a7c1f404a377f03591f27157e5e
NikoTronic <nikorueda2002@gmail.com>
2023-02-01 19:20:24 -0500

    Actualizacion 2

--- prueba/autores/models.py
+++ prueba/autores/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#Modelo para la entidad Autor. 
+class Autor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    pais = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nombre
--- prueba/categorias/models.py
+++ prueba/categorias/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Categoria(models.Model):
-    name= models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
